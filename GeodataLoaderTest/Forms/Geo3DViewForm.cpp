425921152e63aeda0897822d62c9b99e759cf045
galov <hootfat@gmail.com>
2018-04-17 23:54:53 +0500

    added lightning, but normals still messed up

--- GeodataLoaderTest/Forms/Geo3DViewForm.cpp
+++ GeodataLoaderTest/Forms/Geo3DViewForm.cpp
@@ -45,8 +45,16 @@ static ID3D11DepthStencilView *DepthStencilView;
 static XMMATRIX WorldMatrix;
 static XMMATRIX ViewMatrix;
 static XMMATRIX ProjectionMatrix;
-static ID3D11Buffer *ShaderMatrix;
+static ID3D11Buffer *ShaderMatrixRef;
 static ID3D11SamplerState* Sampler;
+
+struct ShaderOptions {
+	float LigthEnabled;
+	float Padding[3];
+};
+
+static ID3D11Buffer *ShaderOptionsRef;
+static ShaderOptions Options;
 
 static ID3D11Buffer *SceneVertexBuffer;
 static ID3D11Buffer *SceneIndexBuffer;
@@ -63,18 +71,19 @@ static uint32_t GeoGridWidth;
 static uint32_t GeoGridHeight;
 static uint32_t GeoGridLayersCount;
 
-#define MAX_VERTICES (10 * 1000 * 1000)
-static InputVertex VertexBuffer[MAX_VERTICES];
+#define MAX_VERTICES (30 * 1000 * 1000)
+static InputVertex *VertexBuffer;
 static uint32_t NextVertexIndex;
 
 #define MAX_INDEXES (MAX_VERTICES * 5)
-static uint32_t IndexBuffer[MAX_INDEXES];
+static uint32_t *IndexBuffer;
 static uint32_t NextIndexIndex;
 static uint32_t LineStartIndex;
 
 #define KEYS_COUNT 256
 static bool PressedKeys[KEYS_COUNT];
 static bool InFocus;
+static float MoveSpeed = 30.0f;
 
 void Geo3DViewForm::Init(unsigned int Width, unsigned int Height, WCHAR *WindowClass, WCHAR *Title,
 	HINSTANCE hInstance) {
@@ -91,7 +100,7 @@ void Geo3DViewForm::Init(unsigned int Width, unsigned int Height, WCHAR *WindowC
 	if (WindowHandle == 0)
 		throw new std::runtime_error("Couldn't create window");
 
-	DXGI_SWAP_CHAIN_DESC sd = { };
+	DXGI_SWAP_CHAIN_DESC sd = {};
 	sd.BufferCount = 1;
 	sd.BufferDesc.Width = Width;
 	sd.BufferDesc.Height = Height;
@@ -113,9 +122,9 @@ void Geo3DViewForm::Init(unsigned int Width, unsigned int Height, WCHAR *WindowC
 		throw new std::runtime_error("Couldn't create DX device and swap chain");
 
 	// Setup the viewport
-	D3D11_VIEWPORT vp = { };
-	vp.Width = (float) Width;
-	vp.Height = (float) Height;
+	D3D11_VIEWPORT vp = {};
+	vp.Width = (float)Width;
+	vp.Height = (float)Height;
 	vp.MinDepth = 0;
 	vp.MaxDepth = 1;
 	vp.TopLeftX = 0;
@@ -130,9 +139,14 @@ void Geo3DViewForm::Init(unsigned int Width, unsigned int Height, WCHAR *WindowC
 
 	// compiling and creating vertex shader
 	ID3DBlob *VSBlob;
-	res = D3DCompile(ShaderData, ShaderSize, NULL, NULL, NULL, "VS", "vs_4_0", D3DCOMPILE_ENABLE_STRICTNESS, 0, &VSBlob, NULL);
-	if (FAILED(res))
+	ID3DBlob *Errors;
+	res = D3DCompile(ShaderData, ShaderSize, NULL, NULL, NULL, "VS", "vs_4_0", D3DCOMPILE_ENABLE_STRICTNESS, 0, &VSBlob, &Errors);
+	if (FAILED(res)) {
+
+		string ErrorText = string((const char *)Errors->GetBufferPointer(), Errors->GetBufferSize());
+
 		throw new std::runtime_error("Couldn't compile vertex shader");
+	}
 
 	res = DirectDevice->CreateVertexShader(VSBlob->GetBufferPointer(), VSBlob->GetBufferSize(), NULL, &VertexShader);
 	if (FAILED(res))
@@ -151,18 +165,18 @@ void Geo3DViewForm::Init(unsigned int Width, unsigned int Height, WCHAR *WindowC
 	// creating input layout
 	static const D3D11_INPUT_ELEMENT_DESC VertexLayoutDesc[3] = {
 		{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0,  0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
-		{ "COLOR",    0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 12, D3D11_INPUT_PER_VERTEX_DATA, 0 },
-		{ "NORMAL",   0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 24, D3D11_INPUT_PER_VERTEX_DATA, 0 }
+	{ "COLOR",    0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 12, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+	{ "NORMAL",   0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 24, D3D11_INPUT_PER_VERTEX_DATA, 0 }
 	};
 
-	res = DirectDevice->CreateInputLayout(VertexLayoutDesc, sizeof(VertexLayoutDesc) / sizeof(*VertexLayoutDesc), 
+	res = DirectDevice->CreateInputLayout(VertexLayoutDesc, sizeof(VertexLayoutDesc) / sizeof(*VertexLayoutDesc),
 		VSBlob->GetBufferPointer(), VSBlob->GetBufferSize(), &VertexLayout);
 	if (FAILED(res))
 		throw new std::runtime_error("Couldn't create input layout");
 
 	// Create a render target view
 	ID3D11Texture2D *BackBuffer;
-	res = SwapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (LPVOID*) &BackBuffer);
+	res = SwapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (LPVOID*)&BackBuffer);
 	if (FAILED(res))
 		throw new std::runtime_error("Couldn't get back buffer");
 
@@ -196,7 +210,7 @@ void Geo3DViewForm::Init(unsigned int Width, unsigned int Height, WCHAR *WindowC
 		throw new std::runtime_error("Couldn't create depth stencil view");
 
 	// creating sampler for textures
-	D3D11_SAMPLER_DESC SamplerDesc = { };
+	D3D11_SAMPLER_DESC SamplerDesc = {};
 	SamplerDesc.Filter = D3D11_FILTER_ANISOTROPIC;
 	SamplerDesc.AddressU = D3D11_TEXTURE_ADDRESS_WRAP;
 	SamplerDesc.AddressV = D3D11_TEXTURE_ADDRESS_WRAP;
@@ -223,7 +237,7 @@ void Geo3DViewForm::Init(unsigned int Width, unsigned int Height, WCHAR *WindowC
 
 	// creating buffers
 
-	D3D11_BUFFER_DESC SceneVertexBufferDesc = { };
+	D3D11_BUFFER_DESC SceneVertexBufferDesc = {};
 	SceneVertexBufferDesc.ByteWidth = sizeof(InputVertex) * MAX_VERTICES;
 	SceneVertexBufferDesc.Usage = D3D11_USAGE_DEFAULT;
 	SceneVertexBufferDesc.BindFlags = D3D11_BIND_VERTEX_BUFFER;
@@ -231,7 +245,7 @@ void Geo3DViewForm::Init(unsigned int Width, unsigned int Height, WCHAR *WindowC
 	if (FAILED(res))
 		throw new std::runtime_error("Couldn't create vertex buffer");
 
-	D3D11_BUFFER_DESC SceneIndexBufferDesc = { };
+	D3D11_BUFFER_DESC SceneIndexBufferDesc = {};
 	SceneIndexBufferDesc.ByteWidth = sizeof(uint32_t) * MAX_INDEXES;
 	SceneIndexBufferDesc.Usage = D3D11_USAGE_DEFAULT;
 	SceneIndexBufferDesc.BindFlags = D3D11_BIND_INDEX_BUFFER;
@@ -240,15 +254,25 @@ void Geo3DViewForm::Init(unsigned int Width, unsigned int Height, WCHAR *WindowC
 		throw new std::runtime_error("Couldn't create index buffer");
 
 	// create shader variable reference
-	D3D11_BUFFER_DESC MatrixDesc = { };
+	D3D11_BUFFER_DESC MatrixDesc = {};
 	MatrixDesc.Usage = D3D11_USAGE_DEFAULT;
 	MatrixDesc.ByteWidth = sizeof(XMFLOAT4X4);
 	MatrixDesc.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
-	res = DirectDevice->CreateBuffer(&MatrixDesc, NULL, &ShaderMatrix);
+	res = DirectDevice->CreateBuffer(&MatrixDesc, NULL, &ShaderMatrixRef);
 	if (FAILED(res))
 		throw new std::runtime_error("Couldn't create shader matrix buffer");
 
-	DirectDeviceCtx->VSSetConstantBuffers(0, 1, &ShaderMatrix);
+	DirectDeviceCtx->VSSetConstantBuffers(0, 1, &ShaderMatrixRef);
+
+	D3D11_BUFFER_DESC OptionsDesc = {};
+	OptionsDesc.Usage = D3D11_USAGE_DEFAULT;
+	OptionsDesc.ByteWidth = sizeof(ShaderOptions);
+	OptionsDesc.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
+	res = DirectDevice->CreateBuffer(&OptionsDesc, NULL, &ShaderOptionsRef);
+	if (FAILED(res))
+		throw new std::runtime_error("Couldn't create shader options buffer");
+
+	DirectDeviceCtx->PSSetConstantBuffers(1, 1, &ShaderOptionsRef);
 
 	// setup frame
 	DirectDeviceCtx->OMSetRenderTargets(1, &RenderTargetView, DepthStencilView);
@@ -275,26 +299,42 @@ void Geo3DViewForm::Init(unsigned int Width, unsigned int Height, WCHAR *WindowC
 
 	// setup matrices
 
-	ProjectionMatrix = XMMatrixPerspectiveFovLH(0.5f * (float)M_PI, (float)Width / (float)Height, 1.0f, 1000.0f);
+	ProjectionMatrix = XMMatrixPerspectiveFovLH(0.5f * (float)M_PI, (float)Width / (float)Height, 0.1f, 1000.0f);
 
 	BuildViewMatrix();
 	BuildWorldMatrix();
 
 	BuildShaderMatrix();
 
+	VertexBuffer = new InputVertex[MAX_VERTICES];
+	IndexBuffer = new uint32_t[MAX_INDEXES];
+
 	// GenerateDebugStaticScene();
 
-	/*
+
 	int16_t Count;
-	L2Geodata::GetLayeredSubBlocks(13100, 140572, Count)[0] = -2700 << 1;
-	L2Geodata::GetLayeredSubBlocks(13100 + 16, 140572, Count)[0] = -2700 << 1;
-	L2Geodata::GetLayeredSubBlocks(13100, 140572 + 16, Count)[0] = -2692 << 1;
-	L2Geodata::GetLayeredSubBlocks(13100 + 16, 140572 + 16, Count)[0] = -2700 << 1;
-	 */
-	GenerateGeodataScene(13100, 140572, 1600, 1600);
+	for (int X = -10; X <= 10; X++)
+		for (int Y = -10; Y <= 10; Y++) {
+
+			int Offset = abs(X) | abs(Y);
+
+			L2Geodata::GetLayeredSubBlocks(13100 + X * 16, 140572 + Y * 16, Count)[0] = -2732 << 1;
+		}
+
+	L2Geodata::GetLayeredSubBlocks(13100 + 16, 140572 + 16, Count)[0] = -2724 << 1;
+	L2Geodata::GetLayeredSubBlocks(13100 + 16, 140572 + 32, Count)[0] = -2716 << 1;
+	// L2Geodata::GetLayeredSubBlocks(13100 + 32, 140572 + 16, Count)[0] = -2716 << 1;
+	// L2Geodata::GetLayeredSubBlocks(13100 + 32, 140572 + 32, Count)[0] = -2708 << 1;
+	/*
+	L2Geodata::GetLayeredSubBlocks(13100 + 16, 140572, Count)[0] = -2692 << 1;
+	L2Geodata::GetLayeredSubBlocks(13100, 140572 + 16, Count)[0] = -2700 << 1;
+	L2Geodata::GetLayeredSubBlocks(13100 + 16, 140572 + 16, Count)[0] = -2684 << 1;
+	*/
+	
+	GenerateGeodataScene(13100, 140572, 64, 64);
 
 	// register raw input
-	RAWINPUTDEVICE Rid[1] = { };
+	RAWINPUTDEVICE Rid[1] = {};
 
 	Rid[0].usUsagePage = 0x01;
 	Rid[0].usUsage = 0x02;
@@ -324,7 +364,7 @@ void Geo3DViewForm::BuildWorldMatrix(void)
 	XMVECTOR zero = XMVectorSet(0.0f, 0.0f, 0.0f, 1.0f);
 	XMVECTOR one = XMVectorSet(1.0f, 1.0f, 1.0f, 1.0f);
 	XMVECTOR position = XMVectorSet(CameraPosition.x, CameraPosition.y, CameraPosition.z, 0.0f);
-	
+
 	WorldMatrix = XMMatrixAffineTransformation(one, zero, zero, position);
 }
 
@@ -332,7 +372,7 @@ void Geo3DViewForm::BuildShaderMatrix(void)
 {
 	XMMATRIX FinalMatrix = XMMatrixTranspose(ViewMatrix * ProjectionMatrix);
 
-	DirectDeviceCtx->UpdateSubresource(ShaderMatrix, 0, NULL, &FinalMatrix, 0, 0);
+	DirectDeviceCtx->UpdateSubresource(ShaderMatrixRef, 0, NULL, &FinalMatrix, 0, 0);
 }
 
 uint32_t Geo3DViewForm::AllocateVertexIndex(void)
@@ -380,12 +420,12 @@ void Geo3DViewForm::CommitScene(void) {
 }
 
 void Geo3DViewForm::GenerateDebugStaticScene(void) {
-	
+
 	uint32_t Vertices[4];
-	Vertices[0] = AllocateVertexIndex(); 
-	Vertices[1] = AllocateVertexIndex(); 
-	Vertices[2] = AllocateVertexIndex(); 
-	Vertices[3] = AllocateVertexIndex(); 
+	Vertices[0] = AllocateVertexIndex();
+	Vertices[1] = AllocateVertexIndex();
+	Vertices[2] = AllocateVertexIndex();
+	Vertices[3] = AllocateVertexIndex();
 
 	VertexBuffer[Vertices[0]] = InputVertex(0.0f, 0.0f, -1.0f, 0, 0, 0);
 	VertexBuffer[Vertices[1]] = InputVertex(1.0f, 0.0f, -1.0f, 0, 1, 0);
@@ -444,7 +484,7 @@ int32_t Geo3DViewForm::GetGridVertexIndex(uint32_t GridX, uint32_t GridY, uint32
 		Ret = AllocateVertexIndex();
 		*Index = Ret;
 	}
-	
+
 	return Ret;
 }
 
@@ -520,6 +560,19 @@ template <typename T> int Sign(T val) {
 	return (T(0) < val) - (val < T(0));
 }
 
+float GetRandomCoord(void) {
+
+	float t = (float)rand() / RAND_MAX;
+	t = t * 2.0f - 1.0f;
+
+	return t;
+}
+
+XMVECTOR GetRandomVector(void) {
+
+	return XMVector3Normalize(XMVectorSet(GetRandomCoord(), GetRandomCoord(), GetRandomCoord(), 1.0f));
+}
+
 #define NOT_DIR(x) (1 - abs(x))
 
 void Geo3DViewForm::GenerateGeodataScene(int32_t WorldX, int32_t WorldY, uint32_t Width, uint32_t Height)
@@ -531,7 +584,7 @@ void Geo3DViewForm::GenerateGeodataScene(int32_t WorldX, int32_t WorldY, uint32_
 
 	AllocateGrid(WorldX, WorldY, GridWidth, GridHeight, L2Geodata::LAYERS_PER_SUBBLOCK_LIMIT);
 
-	for (int GridX = 0; GridX < GridWidth; GridX++) 
+	for (int GridX = 0; GridX < GridWidth; GridX++)
 		for (int GridY = 0; GridY < GridHeight; GridY++) {
 
 			int16_t LayerCount;
@@ -567,67 +620,78 @@ void Geo3DViewForm::GenerateGeodataScene(int32_t WorldX, int32_t WorldY, uint32_
 				int32_t VerticesIndexes[2][2];
 
 				// calculate vertices
-				for (int VertexX = 0; VertexX < 2; VertexX++) 
+				for (int VertexX = 0; VertexX < 2; VertexX++)
 					for (int VertexY = 0; VertexY < 2; VertexY++) {
 
 						InputVertex Vertex;
 
-						Vertex.Pos = { (float)(GridX + VertexX), (float)(GridY + VertexY), (float) Height * 0.1f };
-						// Vertex.Color = { 0.0f, 1.0f, 0.0f };
-						Vertex.Color = { (float)rand() / RAND_MAX, (float)rand() / RAND_MAX, (float)rand() / RAND_MAX };
+						Vertex.Pos = { (float)(GridX + VertexX), (float)(GridY + VertexY), (float)Height * 0.1f };
+						Vertex.Color = { 1.0f, 1.0f, 1.0f };
+						// Vertex.Color = { (float)rand() / RAND_MAX, (float)rand() / RAND_MAX, (float)rand() / RAND_MAX };
 						Vertex.Normal = { 0, 0, 0 };
 
-						// if (GridX == 1 && GridY == 0 && VertexX == 0 && VertexY == 1) {
+						// if (GridX == 0 && GridY == 1 && VertexX == 1 && VertexY == 0) {
 
-							// calculate normals
+						// calculate normals
 
-							int NX = VertexX == 0 ? -1 : 1;
-							int NY = VertexY == 0 ? -1 : 1;
+						int NX = VertexX == 0 ? -1 : 1;
+						int NY = VertexY == 0 ? -1 : 1;
 
-							XMVECTOR Normal = { 0, 0, 1 };
+						XMVECTOR Normal = { 0, 0, 1 };
 
-							NeighborInfo *NX_Entry, *NY_Entry, *NXY_Entry;
+						NeighborInfo *NX_Entry, *NY_Entry, *NXY_Entry;
 
-							NX_Entry = &Neighbors[NX + 1][0 + 1];
-							int NtoX_D, NtoY_D, NtoXY_D, NXtoXY_D, NYtoXY_D;
+						NX_Entry = &Neighbors[NX + 1][0 + 1];
+						int NtoX_D, NtoY_D, NtoXY_D, NXtoXY_D, NYtoXY_D;
 
-							if (NX_Entry->LayerIndex != -1)
-								NtoX_D = Sign((int32_t)Height - (int32_t)NX_Entry->Height);
-							else
-								NtoX_D = 0;
+						if (NX_Entry->LayerIndex != -1)
+							NtoX_D = Sign((int32_t)Height - (int32_t)NX_Entry->Height);
+						else
+							NtoX_D = 0;
 
-							NY_Entry = &Neighbors[0 + 1][NY + 1];
-							if (NY_Entry->LayerIndex != -1)
-								NtoY_D = Sign((int32_t)Height - (int32_t)NY_Entry->Height);
-							else
-								NtoY_D = 0;
+						NY_Entry = &Neighbors[0 + 1][NY + 1];
+						if (NY_Entry->LayerIndex != -1)
+							NtoY_D = Sign((int32_t)Height - (int32_t)NY_Entry->Height);
+						else
+							NtoY_D = 0;
 
-							NXY_Entry = &Neighbors[NX + 1][NY + 1];
-							if (NXY_Entry->LayerIndex != -1)
-								NtoXY_D = Sign((int32_t)Height - (int32_t)NXY_Entry->Height);
-							else
-								NtoXY_D = 0;
+						NXY_Entry = &Neighbors[NX + 1][NY + 1];
+						if (NXY_Entry->LayerIndex != -1)
+							NtoXY_D = Sign((int32_t)Height - (int32_t)NXY_Entry->Height);
+						else
+							NtoXY_D = 0;
 
-							if (NX_Entry->LayerIndex != -1 && NXY_Entry->LayerIndex != -1)
-								NXtoXY_D = Sign((int32_t)NX_Entry->Height - (int32_t)NXY_Entry->Height);
-							else
-								NXtoXY_D = 0;
+						if (NX_Entry->LayerIndex != -1 && NXY_Entry->LayerIndex != -1)
+							NXtoXY_D = Sign((int32_t)NX_Entry->Height - (int32_t)NXY_Entry->Height);
+						else
+							NXtoXY_D = 0;
 
-							if (NY_Entry->LayerIndex != -1 && NXY_Entry->LayerIndex != -1)
-								NYtoXY_D = Sign((int32_t)NY_Entry->Height - (int32_t)NXY_Entry->Height);
-							else
-								NYtoXY_D = 0;
+						if (NY_Entry->LayerIndex != -1 && NXY_Entry->LayerIndex != -1)
+							NYtoXY_D = Sign((int32_t)NY_Entry->Height - (int32_t)NXY_Entry->Height);
+						else
+							NYtoXY_D = 0;
 
-							Normal += { (float)(NtoX_D * NX), 0, (float)NOT_DIR(NtoX_D) };
-							Normal += { 0, (float)(NtoY_D * NY), (float)NOT_DIR(NtoY_D) };
+						
+						Normal += { (float)(NtoX_D * NX), 0, (float)NOT_DIR(NtoX_D) };
+						Normal += { 0, (float)(NtoY_D * NY), (float)NOT_DIR(NtoY_D) };
+						/*
+						Normal += { (float)(NtoX_D * NX), 0, 0 };
+						Normal += { 0, (float)(NtoY_D * NY), 0 };
+						*/
 
-							if (NtoY_D != NtoX_D || NtoY_D == 0) {
-								Normal += { (float)(NYtoXY_D * NX), (float)(NXtoXY_D * NY), (float)NOT_DIR(NtoXY_D) };
-							}
+						if (abs(NtoY_D + NtoX_D) < 2) {
+							Normal += { 0, 0, (float)NOT_DIR(NtoXY_D) };
+							if (NtoXY_D != NtoY_D)
+								Normal += { (float)(NYtoXY_D * NX), 0, 0 };
+							if (NtoXY_D != NtoX_D)
+								Normal += { 0, (float)(NXtoXY_D * NY), 0 };
+						}
 
-							Normal = XMVector3Normalize(Normal);
+						// Normal += GetRandomVector() * 1.5f;
 
-							XMStoreFloat3(&Vertex.Normal, Normal);
+						Normal = XMVector3Normalize(Normal);
+
+						XMStoreFloat3(&Vertex.Normal, Normal);
 						// }
 
 						int32_t Index = GetGridVertexIndex(GridX, GridY, LayerIndex, VertexX, VertexY);
@@ -640,7 +704,7 @@ void Geo3DViewForm::GenerateGeodataScene(int32_t WorldX, int32_t WorldY, uint32_
 				vertices
 				0 2
 				1 3
-				 */
+				*/
 
 				// setup top part of quad
 
@@ -701,12 +765,14 @@ void Geo3DViewForm::GenerateGeodataScene(int32_t WorldX, int32_t WorldY, uint32_
 		XMVECTOR Normal = XMLoadFloat3(&Vertex->Normal);
 
 		XMStoreFloat3(&VertexBuffer[L1].Pos, Position);
-		VertexBuffer[L1].Color = { 1, 0, 0 };
+		VertexBuffer[L1].Color = Vertex->Normal;
+		VertexBuffer[L1].Normal = { 0, 0, 1 };
 
 		Position += Normal * 0.5;
 
 		XMStoreFloat3(&VertexBuffer[L2].Pos, Position);
-		VertexBuffer[L2].Color = { 1, 0, 0 };
+		VertexBuffer[L2].Color = Vertex->Normal;
+		VertexBuffer[L2].Normal = { 0, 0, 1 };
 	}
 
 	CommitScene();
@@ -731,9 +797,13 @@ LRESULT Geo3DViewForm::WndProc(HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam
 {
 	switch (Msg)
 	{
-	case WM_SETFOCUS: 
+	case WM_SETFOCUS:
 		RECT Rect;
+		POINT Center;
+
 		GetWindowRect(WindowHandle, &Rect);
+		Center = { Rect.left + (Rect.right - Rect.left) / 2, Rect.top + (Rect.bottom - Rect.top) / 2 };
+		Rect = { Center.x, Center.y, Center.x + 1, Center.y + 1 };
 		ClipCursor(&Rect);
 		InFocus = true;
 		break;
@@ -744,7 +814,7 @@ LRESULT Geo3DViewForm::WndProc(HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam
 		UINT RimType;
 		HRAWINPUT RawHandle;
 		RAWINPUT RawInput;
-	    UINT RawInputSize, Res;
+		UINT RawInputSize, Res;
 
 		RimType = GET_RAWINPUT_CODE_WPARAM(wParam);
 		RawHandle = HRAWINPUT(lParam);
@@ -760,7 +830,7 @@ LRESULT Geo3DViewForm::WndProc(HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam
 
 		if (InFocus)
 			ProcessMouseInput(RawInput.data.mouse.lLastX, RawInput.data.mouse.lLastY);
-		
+
 		break;
 	case WM_SETCURSOR:
 		SetCursor(0);
@@ -770,12 +840,15 @@ LRESULT Geo3DViewForm::WndProc(HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam
 		if (wParam == VK_ESCAPE)
 			PostQuitMessage(0);
 		else
-		if (wParam < KEYS_COUNT)
-			PressedKeys[wParam] = true;
+			if (wParam < KEYS_COUNT)
+				PressedKeys[wParam] = true;
 		break;
 	case WM_KEYUP:
 		if (wParam < KEYS_COUNT)
 			PressedKeys[wParam] = false;
+		break;
+	case WM_MOUSEWHEEL:
+		MoveSpeed = max(1.0f, MoveSpeed + GET_WHEEL_DELTA_WPARAM(wParam) / 120.0f * 1.5f);
 		break;
 	case WM_DESTROY:
 		PostQuitMessage(0);
@@ -794,12 +867,10 @@ void Geo3DViewForm::Show(void) {
 
 void Geo3DViewForm::ProcessKeyboardInput(double dt)
 {
-	float MoveSpeed = 15.0f;
-
 	XMVECTOR SideVector = XMVector3Normalize(XMVector3Cross(-TargetVector, Up));
 	XMVECTOR ForwardVector = XMVector3Cross(SideVector, Up);
 
-	XMVECTOR Displacement = { };
+	XMVECTOR Displacement = {};
 
 	if (PressedKeys['D'])
 		Displacement += SideVector;
@@ -814,7 +885,7 @@ void Geo3DViewForm::ProcessKeyboardInput(double dt)
 	if (PressedKeys[VK_SPACE] || PressedKeys[VK_CONTROL])
 		Displacement += Up;
 
-	Displacement = XMVector3Normalize(Displacement) * (float) (MoveSpeed * dt);
+	Displacement = XMVector3Normalize(Displacement) * (float)(MoveSpeed * dt);
 
 	if (XMVector3LengthSq(Displacement).m128_f32[0] <= 0.0)
 		return;
@@ -830,14 +901,21 @@ void Geo3DViewForm::ProcessKeyboardInput(double dt)
 void Geo3DViewForm::DrawScene(void)
 {
 	// clear the back buffer to a deep blue
-	float color[4] = { 0.0f, 0.0f, 1.0f, 1.0f };
+	float color[4] = { 0.0f, 0.0f, 0.0f, 1.0f };
 	DirectDeviceCtx->ClearRenderTargetView(RenderTargetView, color);
 	DirectDeviceCtx->ClearDepthStencilView(DepthStencilView, D3D11_CLEAR_DEPTH | D3D11_CLEAR_STENCIL, 1.0f, 0);
+
+	Options.LigthEnabled = 1;
+	DirectDeviceCtx->UpdateSubresource(ShaderOptionsRef, 0, NULL, &Options, 0, 0);
 
 	DirectDeviceCtx->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
 	DirectDeviceCtx->DrawIndexed(NextIndexIndex, 0, 0);
 
 	if (LineStartIndex > 0) {
+
+		Options.LigthEnabled = 0;
+		DirectDeviceCtx->UpdateSubresource(ShaderOptionsRef, 0, NULL, &Options, 0, 0);
+
 		DirectDeviceCtx->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_LINELIST);
 		DirectDeviceCtx->Draw(NextVertexIndex - LineStartIndex, LineStartIndex);
 	}
--- GeodataLoaderTest/texture_shader.fx
+++ GeodataLoaderTest/texture_shader.fx
@@ -3,6 +3,11 @@ cbuffer Transformation : register( b0 )
     matrix FinalMatrix;
 };
 
+cbuffer Options : register( b1 )
+{
+    float LightEnabled;
+};
+
 //--------------------------------------------------------------------------------------
 struct VS_INPUT
 {
@@ -15,6 +20,7 @@ struct PS_INPUT
 {
     float4 Pos : SV_POSITION;
     float4 Color : COLOR;
+    float3 Normal : NORMAL;
 };
 
 
@@ -26,6 +32,7 @@ PS_INPUT VS( VS_INPUT input )
     PS_INPUT output = (PS_INPUT)0;
     output.Pos = mul(float4(input.Pos, 1.0f), FinalMatrix);
     output.Color = float4(input.Color, 1.0f);
+    output.Normal = input.Normal;
 
     return output;
 }
@@ -36,5 +43,38 @@ PS_INPUT VS( VS_INPUT input )
 //--------------------------------------------------------------------------------------
 float4 PS( PS_INPUT input) : SV_Target
 {
-    return input.Color;
+    if (LightEnabled < 0.5)
+        return input.Color;
+
+    float4 ambientColor = float4(0.125f, 0.125f, 0.75f, 1.0f);
+    float4 diffuseColor = float4(0.5f, 0.5f, 75.0f, 1.0f);
+    float3 lightDirection = float3(0.0f, 0.0f, -1.0f);
+
+    float4 textureColor = input.Color;
+    float3 lightDir;
+    float lightIntensity;
+    float4 color;
+
+    // Set the default output color to the ambient light value for all pixels.
+    color = ambientColor;
+    
+    // Invert the light direction for calculations.
+    lightDir = -lightDirection;
+
+    // Calculate the amount of light on this pixel.
+    lightIntensity = saturate(dot(input.Normal, lightDir));
+    
+    if(lightIntensity > 0.0f)
+    {
+        // Determine the final diffuse color based on the diffuse color and the amount of light intensity.
+        color += (diffuseColor * lightIntensity);
+    }
+
+    // Saturate the final light color.
+    color = saturate(color);
+
+    // Multiply the texture pixel and the final diffuse color to get the final pixel color result.
+    color = color * textureColor;
+
+    return color;
 }
